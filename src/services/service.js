50edd09cad4da49cae5f83781e04dd5018a5bc50
04amanrajj <04.aman.raj@gmail.com>
2025-03-12 23:11:13 +0530

    Refactor API service to allow dynamic URL fetching and add menu data retrieval

--- src/services/service.js
+++ src/services/service.js
@@ -1,10 +1,19 @@
 import axios from "axios";
-const baseUrl = "https://satyanaam-food-backend.onrender.com/";
-export const getData = async () => {
+// Base URL for the API
+const baseUrl = "https://satyanaam-food-backend.onrender.com";
+export const getData = async (url) => {
     try {
-        const response = await axios.get(baseUrl);
+        const response = await axios.get(url ? url : baseUrl);
         return response.data;
     } catch (error) {
-        console.error(`error : ${error.message}`);
+        console.error(`api fetch error : ${error.message}`);
+    }
+}
+export const menuData = async () => {
+    try {
+        const response = await axios.get(baseUrl + "/menu");
+        return response.data;
+    } catch (error) {
+        console.error(`api fetch error : ${error.message}`);
     }
 }
