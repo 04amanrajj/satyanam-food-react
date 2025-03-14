283817f12e81b540c7aa159006f5ed1d5301710c
04amanrajj <04.aman.raj@gmail.com>
2025-03-15 00:07:04 +0530

    Implement routing and context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RestaurantProvider } from "./contexts/RestaurantContext"; // Import Context Provider
 import Navbar from './components/Navbar';
-import Landing from './components/Landing';
-import './styles/global.css';
 import Products from './components/Products';
-import {  useState } from 'react';
+import Home from './pages/Home';
+import Profile from "./pages/Profile";
+import Menu from "./pages/Menu";
+import Checkout from "./pages/Checkout";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import NotFound from "./pages/notfound";
+import './styles/global.css';
 
 function App() {
-  const [showMenu, setShowMenu] = useState([false]);
-  console.log(showMenu)
+
   return (
-    <>
-      <Navbar />
-      <Landing showMenu={showMenu} setShowMenu={setShowMenu} />
-      {showMenu && <Products />}
-      </>
-  )
+    <RestaurantProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/user" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </RestaurantProvider>
+  );
 }
 
 export default App;
