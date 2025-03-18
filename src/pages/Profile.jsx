6f1e043d5a25c3ab45e0f48a2520c0a706ec002e
04amanrajj <04.aman.raj@gmail.com>
2025-03-18 22:04:19 +0530

    refactor: consolidate authentication forms into a single AuthForm component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import CreateAccount from "../components/auth/CreateAccount";
-import LoginForm from "../components/auth/LoginForm";
+import AuthForm from "../components/AuthForm";
 
 const Profile = () => {
   const location = useLocation();
 
   return (
     <div className="bg--primary p-6">
-      {location.pathname === "/login" ? <LoginForm /> : <CreateAccount />}
+      <AuthForm />
     </div>
   );
 };
-
 export default Profile;
