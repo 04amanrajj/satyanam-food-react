07db9f898f01ac193db0551211391d80e5c63a01
04amanrajj <04.aman.raj@gmail.com>
2025-04-07 23:52:04 +0530

    feat: implement dark mode toggle and update routing components to support theme

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,32 @@ import About from "./pages/About";
 import Footer from "./components/Footer";
 import NotFound from "./pages/notfound";
 import './styles/global.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(localStorage.getItem("darkmode") || false);
+
+  useEffect(() => {
+    const theme = darkMode ? true : false;
+    document.documentElement.dataset.theme = theme;
+    localStorage.setItem("darkmode", theme);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <RestaurantProvider>
       <Router>
-        <Navbar />
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} toggleDarkMode={toggleDarkMode} />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/user" element={<Profile />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home darkMode={darkMode} setDarkMode={setDarkMode} toggleDarkMode={toggleDarkMode} />} />
+          <Route path="/menu" element={<Menu darkMode={darkMode} setDarkMode={setDarkMode} toggleDarkMode={toggleDarkMode} />} />
+          <Route path="/user" element={<Profile darkMode={darkMode} setDarkMode={setDarkMode} toggleDarkMode={toggleDarkMode} />} />
+          <Route path="/about" element={<About darkMode={darkMode} setDarkMode={setDarkMode} toggleDarkMode={toggleDarkMode} />} />
+          <Route path="/checkout" element={<Checkout darkMode={darkMode} setDarkMode={setDarkMode} toggleDarkMode={toggleDarkMode} />} />
+          <Route path="*" element={<NotFound darkMode={darkMode} setDarkMode={setDarkMode} toggleDarkMode={toggleDarkMode} />} />
         </Routes>
       </Router>
       <Footer />
