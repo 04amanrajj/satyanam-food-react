3662f50b241e0fed957f320e3e4052c9d052c10c
04amanrajj <04.aman.raj@gmail.com>
2026-04-01 11:00:00 +0530

    refactor: remove unused Location hook from Profile.jsx to fix ESLint warning

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 const Profile = () => {
-  const location = useLocation();
 
   return (
     <div className="bg--primary p-6">
